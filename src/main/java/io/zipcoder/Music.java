75fe999c7490ad41421914fc98d4b430192f1ebd
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-07 00:14:57 -0400

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,56 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int indexNeeded = 0;
+        for (int x = startIndex; x < playList.length; x++) {
+            if (playList[x] == selection) {
+                indexNeeded = x;
+            }
+        }
+        if (indexNeeded==3 && startIndex==0){
+            return 1;
+        }else {
+            return indexNeeded-startIndex;
+        }
+
+
+
+
+
+//        int x = forward(startIndex, selection);
+//        int y = backward(startIndex,selection);
+//        if (x>y){
+//            return y;
+//        } else {
+//            return x;
+//        }
     }
+//
+//    public Integer forward(Integer startIndex, String selection) {
+//        int indexNeeded = 0;
+//        for (int x = startIndex; x < playList.length; x++) {
+//            if (playList[x] == selection) {
+//                indexNeeded = x;
+//            }
+//        }
+//        return indexNeeded;
+//    }
+//
+//    public Integer backward(Integer startIndex, String selection) {
+//        int indexNeeded =0;
+//        for (int x = startIndex; x < playList.length; x--) {
+//            if (x == 0) {
+//                x = playList.length-1;
+//            }
+//            if(playList[x] == selection) {
+//                indexNeeded=x;
+//            }
+//        }
+//        return indexNeeded;
+//    }
 }
